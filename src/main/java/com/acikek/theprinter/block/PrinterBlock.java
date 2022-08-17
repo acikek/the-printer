92cde4e127b3376121c31bd602f7eeea90feff69
acikek <spiralixp@gmail.com>
2022-08-16 17:46:49 -0700

    handle printer breaking

--- src/main/java/com/acikek/theprinter/block/PrinterBlock.java
+++ src/main/java/com/acikek/theprinter/block/PrinterBlock.java
@@ -6,25 +6,23 @@ import net.minecraft.block.*;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityTicker;
 import net.minecraft.block.entity.BlockEntityType;
-import net.minecraft.entity.ExperienceOrbEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.item.ItemStack;
+import net.minecraft.network.packet.s2c.play.StopSoundS2CPacket;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.BooleanProperty;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.Rarity;
+import net.minecraft.util.*;
+import net.minecraft.util.collection.DefaultedList;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
-import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
@@ -33,6 +31,7 @@ import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 import org.quiltmc.qsl.block.extensions.api.QuiltBlockSettings;
 import org.quiltmc.qsl.item.setting.api.QuiltItemSettings;
+import org.quiltmc.qsl.networking.api.PlayerLookup;
 
 public class PrinterBlock extends HorizontalFacingBlock implements BlockEntityProvider {
 
@@ -83,11 +82,7 @@ public class PrinterBlock extends HorizontalFacingBlock implements BlockEntityPr
 				}
 				else if (state.get(ON)) {
 					world.setBlockState(pos, state.with(ON, false));
-					if (blockEntity.xp > 0 && world instanceof ServerWorld serverWorld) {
-						Vec3d xpPos = Vec3d.ofCenter(pos).add(0.0, 0.4, 0.0);
-						ExperienceOrbEntity.spawn(serverWorld, xpPos, blockEntity.xp);
-					}
-					blockEntity.removeItem(player, false);
+					blockEntity.removeItem(world, pos, player, false);
 					event = ModSoundEvents.SHUTDOWN;
 				}
 			}
@@ -97,6 +92,27 @@ public class PrinterBlock extends HorizontalFacingBlock implements BlockEntityPr
 			}
 		}
 		return ActionResult.PASS;
+	}
+
+	public static void stopPrintingSound(ServerWorld world, BlockPos pos) {
+		StopSoundS2CPacket packet = new StopSoundS2CPacket(ModSoundEvents.PRINTING.getId(), SoundCategory.BLOCKS);
+		for (ServerPlayerEntity player : PlayerLookup.tracking(world, pos)) {
+			player.networkHandler.sendPacket(packet);
+		}
+	}
+
+	@Override
+	public void onStateReplaced(BlockState state, World world, BlockPos pos, BlockState newState, boolean moved) {
+		if (state.getBlock() != newState.getBlock() && world.getBlockEntity(pos) instanceof PrinterBlockEntity blockEntity) {
+			ItemScatterer.spawn(world, pos, DefaultedList.copyOf(ItemStack.EMPTY, blockEntity.getStack(0)));
+			if (world instanceof ServerWorld serverWorld) {
+				blockEntity.tryDropXP(serverWorld, pos);
+				if (state.get(PRINTING)) {
+					stopPrintingSound(serverWorld, pos);
+				}
+			}
+			world.removeBlockEntity(pos);
+		}
 	}
 
 	@Nullable
