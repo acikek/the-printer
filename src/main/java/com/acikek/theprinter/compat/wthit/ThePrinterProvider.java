5bca8950850281567f441bcb7940332f225fd12e
acikek <spiralixp@gmail.com>
2023-01-26 10:23:40 -0800

    wthit support

---
+++ src/main/java/com/acikek/theprinter/compat/wthit/ThePrinterProvider.java
@@ -0,0 +1,48 @@
+package com.acikek.theprinter.compat.wthit;
+
+import com.acikek.theprinter.block.PrinterBlockEntity;
+import mcp.mobius.waila.api.*;
+import mcp.mobius.waila.api.component.ItemComponent;
+import mcp.mobius.waila.api.component.ProgressArrowComponent;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.text.Text;
+
+public class ThePrinterProvider implements IBlockComponentProvider, IServerDataProvider<PrinterBlockEntity> {
+
+	public static ThePrinterProvider INSTANCE = new ThePrinterProvider();
+
+	@Override
+	public void appendBody(ITooltip tooltip, IBlockAccessor accessor, IPluginConfig config) {
+		if (config.getBoolean(ThePrinterWailaPlugin.PRINTER_INFO)) {
+			NbtCompound data = accessor.getServerData();
+			if (data.contains("WInput")) {
+				ITooltipLine line = tooltip.addLine()
+						.with(new ItemComponent(ItemStack.fromNbt(data.getCompound("WInput"))));
+				if (data.contains("WProgress")) {
+					line.with(new ProgressArrowComponent(data.getFloat("WProgress")));
+				}
+				else if (data.contains("WXP")) {
+					line.with(Text.Serializer.fromJson(data.getString("WXP")));
+				}
+			}
+		}
+	}
+
+	@Override
+	public void appendServerData(NbtCompound data, IServerAccessor<PrinterBlockEntity> accessor, IPluginConfig config) {
+		if (config.getBoolean(ThePrinterWailaPlugin.PRINTER_INFO)) {
+			PrinterBlockEntity printer = accessor.getTarget();
+			if (!printer.items.isEmpty()) {
+				data.put("WInput", printer.items.get(0).copy().writeNbt(new NbtCompound()));
+			}
+			if (printer.progress > 0) {
+				data.putFloat("WProgress", (float) printer.progress / printer.requiredTicks);
+			}
+			else if (printer.isXPRequired(accessor.getWorld()) && printer.requiredXP > 0) {
+				Text text = Text.literal("XP: " + (printer.xp + "/" + printer.requiredXP));
+				data.putString("WXP", Text.Serializer.toJson(text));
+			}
+		}
+	}
+}
